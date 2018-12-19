8ddce749debfd4933ea51ccd55c651ce5d7950dd
Tu Hu <tu@jingyaohu.com>
2018-12-19 05:14:34 +0100

    update

--- matlab/arrangedataneg.m
+++ matlab/arrangedataneg.m
@@ -1,10 +1,10 @@
 %% Section 1-Read pre-processed data
 filename = 'serumNEG.xlsx'; % pre-processed data
-SampleList = 'newsamplelist.xlsx'; % sample list label of the cdf files
+SampleList = 'samplelist.xlsx'; % sample list label of the cdf files
 mode = 'neg'; %data mode
 dataform = '01.cdf'; %data format 
 peakInt = 'peak height'; %intensity of the of the peaks
-no_split_for_codes = 2; %split. seems useless??
+%no_split_for_codes = 2; %split. seems useless??
 splitter = '_';
 code_names = {'Subject','Time'}; %Labels
 blank = 'NEGbl.mat'
